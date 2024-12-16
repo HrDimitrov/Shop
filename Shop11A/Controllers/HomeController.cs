9c0f883a52828516506ab2a667f34345ff8d1694
HrDimitrov <hristo.dimitrov@pgmtplovdiv.info>
2024-12-16 10:37:13 +0200

    cat add

--- Shop11A/Controllers/HomeController.cs
+++ Shop11A/Controllers/HomeController.cs
@@ -15,9 +15,9 @@ namespace Shop11A.Controllers
 
         public List<Product> products = new List<Product>
         {
-            new Product{ Id = 1, Name = "Tablet 11a", Description = "Soper mega gega tablet 11a", Price = 1000.11M },
-            new Product{ Id = 2, Name = "Tablet 11b", Description = "Soper mega gega tablet 11b", Price = 1234.11M },
-            new Product{ Id = 3, Name = "Tablet 11c", Description = "Soper mega gega tablet 11b", Price = 1500.11M }
+            new Product{ Id = 1, Name = "Tablet 11a", Description = "Super mega giga tablet 11a", Price = 1000.11M, ImageUrl = "/images/t1.jpg" },
+            new Product{ Id = 2, Name = "Tablet 11b", Description = "Super mega giga tablet 11b", Price = 1234.11M, ImageUrl = "/images/t2.jpg" },
+            new Product{ Id = 3, Name = "Tablet 11c", Description = "Super mega giga tablet 11c", Price = 1500.11M, ImageUrl = "/images/t3.jpg" }
         };
 
         public IActionResult Index()
