cb71eb0b975321d5f2bd448e11e2d5893ffd667b
HrDimitrov <hristo.dimitrov@pgmtplovdiv.info>
2025-01-10 10:37:02 +0200

    Add phones and styles

--- Shop11A/Controllers/HomeController.cs
+++ Shop11A/Controllers/HomeController.cs
@@ -17,7 +17,11 @@ namespace Shop11A.Controllers
         {
             new Product{ Id = 1, Name = "Tablet 11a", Description = "Super mega giga tablet 11a", Price = 1000.11M, ImageUrl = "/images/t1.jpg" },
             new Product{ Id = 2, Name = "Tablet 11b", Description = "Super mega giga tablet 11b", Price = 1234.11M, ImageUrl = "/images/t2.jpg" },
-            new Product{ Id = 3, Name = "Tablet 11c", Description = "Super mega giga tablet 11c", Price = 1500.11M, ImageUrl = "/images/t3.jpg" }
+            new Product{ Id = 3, Name = "Tablet 11c", Description = "Super mega giga tablet 11c", Price = 1500.11M, ImageUrl = "/images/t3.jpg" },
+
+            new Product{ Id = 4, Name = "MehanoPhone 11d", Description = "Super mega giga MehanoPhone 11d", Price = 1500.11M, ImageUrl = "/images/p1.jpg" },
+            new Product{ Id = 5, Name = "MehanoPhone 11e", Description = "Super mega giga MehanoPhone 11e", Price = 1600.11M, ImageUrl = "/images/p2.jpg" },
+            new Product{ Id = 6, Name = "MehanoPhone 11f", Description = "Super mega giga MehanoPhone 11f", Price = 1700.11M, ImageUrl = "/images/p3.jpg" }
         };
 
         public IActionResult Index()
